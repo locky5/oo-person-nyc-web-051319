7b85ab76d96f3c8bb5bdb918ac9276f9494fe476
Kevin Dai <daikevin19@yahoo.com>
2019-05-15 15:12:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,12 @@
 class Person
-  attr_accessor :bank_account, :happiness
+  attr_accessor :bank_account, :happiness, :hygiene
   attr_reader :name
   
   def initialize(name, happiness = 8, hygiene = 8)
     @name = name
     @bank_account = 25
     @happiness = happiness # 0 < happiness < 10
+    @hygiene = hygiene
   end
   
 end
